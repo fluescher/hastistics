c4d5f5c2137971b6a2a45b934efb73e08c21e952
Matthias Brun <matthias.brun@students.fhnw.ch>
2012-05-24 13:12:53 +0200

    Add hypergeometric distribution

--- src/Hastistics/Distributions.hs
+++ src/Hastistics/Distributions.hs
@@ -1,10 +1,11 @@
-module Hastistics.Distributions where 
+module Hastistics.Distributions where
 
 import Hastistics.Data hiding ((/), (+))
 
-data BinominalTable = BinominalTable Integer Double
 header :: [String]
 header = ["k", "p"]
+
+data BinominalTable = BinominalTable Integer Double
 
 instance HSTable BinominalTable where
 	headersOf _ = header 
@@ -13,6 +14,17 @@ instance HSTable BinominalTable where
 
 instance Show BinominalTable where
     show = showTable
+
+data HypergeometricTable = HypergeometricTable Integer Integer Integer
+	  
+instance HSTable HypergeometricTable where
+	headersOf _ = header
+	dataOf (HypergeometricTable m r n) = [toHSRow k (hygepdf k m r n) | k <- [0..r]]
+	lookup _ _ _ = []
+	 
+instance Show HypergeometricTable where
+	show = showTable
+
 
 toHSRow :: Integer -> Double -> HSRow
 toHSRow n p = HSValueRow header [pack(HSStaticField(HSInteger n)), pack(HSStaticField(HSDouble p))]
@@ -35,4 +47,10 @@ binopdf k n p	| k < 0 || k > n	= 0
 binocdf :: Integer -> Integer -> Double -> Double
 binocdf k n p 	| k < 0			= 0
 				| k >= n		= 1
-				| otherwise		= sum [binopdf x n p | x <- [0..(k-1)]]
+				| otherwise		= sum [binopdf x n p | x <- [0..k]]
+
+hygepdf :: Integer -> Integer -> Integer -> Integer -> Double
+hygepdf k m r n	| n == 0		= 0 
+				| otherwise 	= ((r `choose` k) * ((m-r) `choose` (n-k))) / (m `choose` n)
+
+hygecdf k m r n = sum [hygepdf x m r n | x <- [0..k]]
