11b0a33ae41b1f1cf188ef9ba026235f5f010bad
Patrick Walther <patrick.walther@students.fhnw.ch>
2012-05-24 15:47:29 +0200

    CSV Parser can now parse given CSV Strings

--- src/Hastistics/Data/CSV.hs
+++ src/Hastistics/Data/CSV.hs
@@ -1,25 +1,30 @@
 module Hastistics.Data.CSV where
 
---import Hastistics.Data
+import Hastistics
+import Hastistics.Types
 import Text.ParserCombinators.Parsec
 
-csvFile :: GenParser Char st [[String]]
-csvFile = endBy line eol
+type CSV = [Record]
+type Record = [Field]
+type Field = String
 
-line :: GenParser Char st [String]
-line = sepBy cell (char ',')
+csv :: Parser CSV
+csv = endBy record eol
 
-cell :: GenParser Char st String
-cell = quotedCell <|> many (noneOf ",\n\r")
+record :: Parser Record
+record = sepBy field (char ',')
 
-quotedCell :: GenParser Char st String
-quotedCell = 
+field :: Parser Field
+field = quotedField <|> many (noneOf ",\n\r")
+
+quotedField :: Parser Field
+quotedField = 
     do _ <- char '"'
        content <- many quotedChar
        _ <- char '"' <?> "quote at end of cell"
        return content
 
-quotedChar :: GenParser Char st Char
+quotedChar :: Parser Char
 quotedChar =
         noneOf "\""
     <|> try (string "\"\"" >> return '"')
@@ -34,19 +39,4 @@ eol =   try (string "\n\r")
 parseCSV :: String -> Either ParseError [[String]]
 parseCSV input = parse csvFile "(unknown)" input
 
-main :: IO()
-main =
-    do c <- getContents
-       case parse csvFile "(stdin)" c of
-            Left e -> do putStrLn "Error parsing input:"
-                         print e
-            Right r -> mapM_ print r
-			
-{-
-data CSVTable = CSVFile [String] [HSCol] [HSRow]
-
-instance HSTable CSVTable where
-        colsOf (CSVFile _ columns _)= columns
-        headersOf (CSVFile h _ _)   = [HSColHeader s | s <-h] 
-        dataOf (CSVFile _ _ values) = values
--}
+--data CSVTable = CSVTable String
