7063b6d819fea5d2026ae75c6f1f9cea605586e3
Patrick Walther <patrick.walther@students.fhnw.ch>
2012-05-31 15:29:11 +0200

    Added definitions for (+)

--- src/Hastistics/Types.hs
+++ src/Hastistics/Types.hs
@@ -37,22 +37,43 @@ toDouble = HSDouble . read
 
 
 (+)     :: HSValue -> HSValue -> HSValue
-(+) (HSString sa)    (HSString sb) = HSString (sa Prelude.++ sb)
-(+) (HSInt ia)       (HSInt ib)    = HSInt (ia Prelude.+ ib)
-(+) (HSDouble da)    (HSDouble db) = HSDouble (da Prelude.+ db)
-(+) (HSDouble da)    (HSInt ib)    = HSDouble (da Prelude.+ (fromIntegral ib))
-(+) (HSInt ia)       (HSDouble db) = HSDouble ((fromIntegral ia) Prelude.+ db)
-(+) (HSInteger ia)   (HSInteger ib)= HSInteger (ia Prelude.+ ib)
-(+) _               _              = None
+(+) (HSString sa)    (HSDouble db)  = HSString (sa Prelude.++ (show db))
+(+) (HSString sa)    (HSInt ib)     = HSString (sa Prelude.++ (show ib))
+(+) (HSString sa)    (HSInteger ib) = HSString (sa Prelude.++ (show ib))
+(+) (HSString sa)    (HSString sb)  = HSString (sa Prelude.++ sb)
+(+) (HSInt ia)       (HSDouble db)  = HSDouble ((fromIntegral ia) Prelude.+ db)
+(+) (HSInt ia)       (HSInt ib)     = HSInt (ia Prelude.+ ib)
+(+) (HSInt ia)       (HSInteger ib) = HSInteger ((fromIntegral ia) Prelude.+ ib)
+(+) (HSInt ia)       (HSString sb)  = HSString ((show ia) Prelude.++ sb)
+(+) (HSInteger ia)   (HSDouble db)  = HSDouble ((fromIntegral ia) Prelude.+ db)
+(+) (HSInteger ia)   (HSInt ib)     = HSInteger (ia Prelude.+ (fromIntegral ib))
+(+) (HSInteger ia)   (HSInteger ib) = HSInteger (ia Prelude.+ ib)
+(+) (HSInteger ia)   (HSString sb)  = HSString ((show ia) Prelude.++ sb)
+(+) (HSDouble da)    (HSDouble db)  = HSDouble (da Prelude.+ db)
+(+) (HSDouble da)    (HSInt ib)     = HSDouble (da Prelude.+ (fromIntegral ib))
+(+) (HSDouble da)    (HSInteger ib) = HSDouble (da Prelude.+ (fromIntegral ib))
+(+) (HSDouble da)    (HSString sb)  = HSString ((show da) Prelude.++ sb)
+(+) _               _               = None
+
+{-
+(-)     :: HSValue -> HSValue -> HSValue
+(-) (HSString sa)    (HSString sb)  = None
+(-) (HSInt ia)       (HSInt ib)     = HSInt (ia Prelude.- ib)
+(-) (HSDouble da)    (HSDouble db)  = HSDouble (da Prelude.- db)
+(-) (HSDouble da)    (HSInt ib)     = HSDouble (da Prelude.- (fromIntegral ib))
+(-) (HSInt ia)       (HSDouble db)  = HSDouble ((fromIntegral ia) Prelude.- db)
+(-) (HSInteger ia)   (HSInteger ib) = HSInteger (ia Prelude.- ib)
+(-) _               _               = None
+-}
 
 (/)     :: HSValue -> HSValue -> HSValue
-(/) _               (HSInt 0)      = None
-(/) _               (HSDouble 0)   = None
-(/) (HSInt ia)      (HSInt ib)     = HSInt (div ia ib)
-(/) (HSDouble da)   (HSDouble db)  = HSDouble (da Prelude./ db)
-(/) (HSDouble da)   (HSInt ib)     = HSDouble (da Prelude./ (fromIntegral ib))
-(/) (HSInt ia)      (HSDouble db)  = HSDouble ((fromIntegral ia) Prelude./ db)
-(/) _               _              = None
+(/) _               (HSInt 0)       = None
+(/) _               (HSDouble 0)    = None
+(/) (HSInt ia)      (HSInt ib)      = HSInt (div ia ib)
+(/) (HSDouble da)   (HSDouble db)   = HSDouble (da Prelude./ db)
+(/) (HSDouble da)   (HSInt ib)      = HSDouble (da Prelude./ (fromIntegral ib))
+(/) (HSInt ia)      (HSDouble db)   = HSDouble ((fromIntegral ia) Prelude./ db)
+(/) _               _               = None
 
 class (Show f) => HSField f where
     val         :: f -> HSValue
@@ -120,7 +141,7 @@ showRow         :: (Show a) => [a] -> String
 showRow []      = "|"
 showRow (k:ks)  = "| " ++ v ++ space ++ " " ++ showRow ks
                 where   v       = take colWidth (show k)
-                        space   = take (colWidth-(length v)) (repeat ' ')
+                        space   = take (colWidth Prelude.- (length v)) (repeat ' ')
 
 showRows        :: [HSRow] -> String
 showRows []     = ""
